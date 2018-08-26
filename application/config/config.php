c66291f6e469df56581319b2df4e58106d827dba
Nico D Note <len_we@hotmail.com>
2018-08-25 21:58:13 -0300

    Cambio base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'localhost/nico/';
+$config['base_url'] = 'https://afternoon-hamlet-13828.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
